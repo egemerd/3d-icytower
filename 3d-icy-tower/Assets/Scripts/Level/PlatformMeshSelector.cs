7177839e3dc97bd8824870cf89cc907d8f3047c1
egemerd <egemertkarsli@hotmail.com>
2026-06-03 01:16:35 +0300

    platform scripts are added. may have a bug of spawning platforms in chunks.

---
+++ 3d-icy-tower/Assets/Scripts/Level/PlatformMeshSelector.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlatformMeshSelector : MonoBehaviour
+{
+    [SerializeField] private GameObject biom01Platform;
+    [SerializeField] private GameObject biom02Platform;
+    [SerializeField] private GameObject biom03Platform;
+
+    private void Start()
+    {
+        int currentBiom = SceneManager.GetActiveScene().buildIndex;
+        SelectPlatformMesh(currentBiom);
+    }
+
+    private void SelectPlatformMesh(int biom)
+    {
+        biom01Platform.SetActive(biom == 0 || biom== 1); 
+        biom02Platform.SetActive(biom == 2);
+        biom03Platform.SetActive(biom == 3);
+    }
+}
